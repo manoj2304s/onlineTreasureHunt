59c139a09950eef27890d67d750e54fe05871cf4
Manoj S <manojyadav23s04@gmail.com>
2026-03-04 22:39:54 +0530

    feat: connect mongodb and implement user registration

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,12 +1,14 @@
 import { Request, Response } from "express";
+import User from "../models/user.model";
 
-export const register = (req: Request, res: Response) => {
-  const { username, email, password } = req.body;
-  res.status(201).json({
-    message: "User registered successfully",
-    user: {
-      username,
-      email,
-    },
-  });
+export const register = async (req: Request, res: Response) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const user = await User.create({ username, email, password });
+    res.status(201).json({ message: "User registered successfully", user });
+  } catch (error) {
+    console.error("Error registering user:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
