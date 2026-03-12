a37f8e236714bebd2461eede0b3e4b7430cef998
Manoj S <manojyadav23s04@gmail.com>
2026-03-12 14:21:16 +0530

    refactor: DB URL hardcoding removed

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/treasurehunt");
+    await mongoose.connect(process.env.DB_URL as string);
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error);
--- backend/src/scripts/seedLevel.ts
+++ backend/src/scripts/seedLevel.ts
@@ -3,21 +3,27 @@ import bcrypt from "bcrypt";
 import Level from "../models/level.model";
 
 async function seedLevels() {
-    await mongoose.connect("mongodb://localhost:27017/treasurehunt") 
+  await mongoose.connect(process.env.DB_URL as string);
 
-    const answer = "echo"
+  const answer = "echo";
 
-    const answerHash = await bcrypt.hash(answer, 10); 
+  const answerHash = await bcrypt.hash(answer, 10);
 
-    await Level.create({
-        levelNumber: 1,
-        question: "I speak without a mouth and hear without ears. I have no body, but I come alive with the wind. What am I?",
-        answerHash: answerHash,
-        hint: "It's a common command in Unix-based systems.",
-    })
+  await Level.create({
+    levelNumber: 1,
+    question:
+      "I speak without a mouth and hear without ears. I have no body, but I come alive with the wind. What am I?",
+    answerHash: answerHash,
+    hint: "It's a common command in Unix-based systems.",
+    qrCode: "QR_START_001",
+    location: {
+      latitude: 13.0254172,
+      longitude: 76.1123468,
+    },
+  });
 
-    console.log("Levels seeded successfully!");
-    process.exit(0);
+  console.log("Levels seeded successfully!");
+  process.exit(0);
 }
 
-seedLevels()
+seedLevels();
